ef7b2bb74b7ebd24de34cf8af8a7a56b89b056cf
Matsushin <matsu1983@gmail.com>
2019-06-01 19:58:01 +0900

    AdminLTEの導入と必要なライブラリ追加

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,22 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+
+environment.plugins.append(
+    'Provide',
+    new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        jquery: 'jquery',
+    })
+)
+environment.loaders.get('sass').use.splice(-1, 0, {
+    loader: 'resolve-url-loader',
+    options: {
+        attempts: 1
+    }
+})
+
+const config = environment.toWebpackConfig()
+
 module.exports = environment
