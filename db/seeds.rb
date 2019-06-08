76dc4ca2ffc325290ddd3f50488a0f76a30270a6
Matsushin <matsu1983@gmail.com>
2019-06-08 14:38:48 +0900

    seedを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Article.transaction do
+  # Article
+  5.times do |article_count|
+    title = "テスト記事#{article_count + 1}"
+    body = "記事内容#{article_count + 1}"
+    article = Article.new(title: title, body: body)
+    article.save!
+
+    # Comment
+    2.times do |comment_count|
+      article.comments.create!(body: "#{title}のコメント#{comment_count + 1}")
+    end
+  end
+end
