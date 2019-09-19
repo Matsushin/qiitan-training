8274c00279e94dc5b5f3f2a0cfafef9b16f68e9c
Matsushin <matsu1983@gmail.com>
2019-09-19 22:57:50 +0900

    課題13：タグをまとめて設定する

--- app/services/articles/update.rb
+++ app/services/articles/update.rb
@@ -3,11 +3,13 @@ class Articles::Update < ApplicationService
   string :title, default: ''
   string :body, default: ''
   file :image, default: nil
+  hash :tags_attributes, strip: false
 
   def execute
     article.title = title
     article.body = body
     article.image.attach(image) if image?
+    article.tags_attributes = tags_attributes
     errors.merge!(article.errors) unless article.save
     article
   end
