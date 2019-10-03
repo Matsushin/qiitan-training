94e2b0829b5e87696b14d2bd811904013dcd185c
Shinichi Matsuda <matsu1983@gmail.com>
2019-10-03 13:11:23 +0900

    Revert "Revert "課題13：タグをまとめて設定する""

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
   has_many :comments
+  has_many :tags, -> { order('display_order ASC') }, dependent: :destroy, inverse_of: :article
+  accepts_nested_attributes_for :tags, reject_if: :all_blank, allow_destroy: true
 
   belongs_to :user
 
