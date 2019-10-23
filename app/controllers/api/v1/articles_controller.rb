cfc4dda87f118d25442fe4dd22f5c575767c84e7
Matsushin <matsu1983@gmail.com>
2019-10-23 11:10:51 +0900

    リファクタ

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,6 +1,4 @@
-class Api::V1::ArticlesController < ApplicationController
-  skip_before_action :authenticate_user!
-
+class Api::V1::ArticlesController < Api::V1::ApplicationController
   def index
     articles = Article.all.page(params[:page]).per(10)
     render json: articles.to_json(only: %i[id title], methods: %i[short_body author_email comment_count created_on])
